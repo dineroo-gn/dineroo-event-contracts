5235542adbfc6370a06a4ba1ae530f032af6ab38
Abou FOFANA <aboufofana76@gmail.com>
2025-06-22 04:34:36 +0200

    publish package

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 plugins {
     kotlin("jvm") version "2.1.20"
+    `maven-publish`
     id("com.github.davidmc24.gradle.plugin.avro") version "1.9.1"
 }
 
 group = "com.cgf.contracts"
 version = "1.0-SNAPSHOT"
+
 
 java {
     toolchain {
@@ -36,5 +38,27 @@ kotlin {
     jvmToolchain(21)
     compilerOptions {
         freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+
+publishing {
+    publications {
+        create<MavenPublication>("avroSchemas") {
+            from(components["java"])
+            groupId = "com.example"
+            artifactId = "avro-schemas"
+            version = "1.0.0"
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/dineroo-gn/dineroo-events-contracts")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
     }
 }
