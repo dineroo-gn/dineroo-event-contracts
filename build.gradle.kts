eafe44e60564c98ff1e0d3ec48937e065692421f
Abou FOFANA <aboufofana76@gmail.com>
2025-06-22 05:21:12 +0200

    publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/dineroo-gn/dineroo-events-contracts")
+            url = uri("https://maven.pkg.github.com/dineroo-gn/dineroo-event-contracts")
             credentials {
                 username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
                 password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
